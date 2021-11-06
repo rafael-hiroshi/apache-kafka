6280031f57c24230526c677475608bf3842c465e
Rafael Hiroshi <rafaelhkp@gmail.com>
2021-11-06 16:19:12 -0300

    List kafka commands

--- kafka.sh
+++ kafka.sh
@@ -1,2 +1,6 @@
+bin/zookeeper-server-start.sh config/zookeeper.properties
+bin/kafka-server-start.sh config/server.properties
+
 bin/kafka-topics.sh --alter --zookeeper localhost:2181 --topic ECOMMERCE_NEW_ORDER --partitions 3
 bin/kafka-topics.sh --bootstrap-server localhost:9092 --describe
+bin/kafka-consumer-groups.sh --all-groups --bootstrap-server localhost:9092 --describe
