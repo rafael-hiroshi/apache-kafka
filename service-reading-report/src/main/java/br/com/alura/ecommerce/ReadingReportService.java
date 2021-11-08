629ac7901ce6833b0541eaa995e9578c1a6f4412
Rafael Hiroshi <rafaelhkp@gmail.com>
2021-11-08 10:43:47 -0300

    Create user reading report service

---
+++ service-reading-report/src/main/java/br/com/alura/ecommerce/ReadingReportService.java
@@ -0,0 +1,36 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+
+public class ReadingReportService {
+
+    private static final Path SOURCE = new File("src/main/resources/report.txt").toPath();
+
+    public static void main(String[] args) {
+        ReadingReportService readingReportService = new ReadingReportService();
+        KafkaService service = new KafkaService<>(ReadingReportService.class.getSimpleName(),
+                "USER_GENERATE_READING_REPORT",
+                readingReportService::parse,
+                Order.class,
+                new HashMap<>());
+        service.run();
+    }
+
+    private void parse(ConsumerRecord<String, User> record) throws IOException {
+        System.out.println("-----------------------------------------");
+        System.out.printf("Processing report for " + record.value());
+
+        User user = record.value();
+        File target = new File(user.getReportPath());
+        IO.copyTo(SOURCE, target);
+        IO.append(target, "Created for " + user.getUuid());
+
+        System.out.println("File created: " + target.getAbsolutePath());
+
+    }
+}
