680b5b3315003625a911dc07801d4c6afa679e0f
Rafael Hiroshi <rafaelhkp@gmail.com>
2021-11-07 22:50:12 -0300

    Add kafka topics command

--- kafka.sh
+++ kafka.sh
@@ -3,4 +3,8 @@ bin/kafka-server-start.sh config/server.properties
 
 bin/kafka-topics.sh --alter --zookeeper localhost:2181 --topic ECOMMERCE_NEW_ORDER --partitions 3
 bin/kafka-topics.sh --bootstrap-server localhost:9092 --describe
+bin/kafka-topics.sh --zookeeper localhost:2181 --describe
 bin/kafka-consumer-groups.sh --all-groups --bootstrap-server localhost:9092 --describe
+
+#Can't alter replication factor after topic has been created
+bin/kafka-topics.sh --zookeeper localhost:2181 --alter --topic ECOMMERCE_NEW_ORDER --partitions 3 --replication-factor 2
