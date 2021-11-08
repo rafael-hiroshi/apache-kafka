61434a177707cadd4d2bc8b84311f4edb11b5d12
Rafael Hiroshi <rafaelhkp@gmail.com>
2021-11-08 19:46:20 -0300

    Remove unused generic class type argument

--- common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -5,34 +5,36 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
-public class KafkaService<T> {
-    private final KafkaConsumer<String, T> consumer;
+public class KafkaService<T> implements Closeable {
+    private final KafkaConsumer<String, Message<T>> consumer;
     private final ConsumerFunction parse;
 
     public KafkaService(String groupId, String topic, ConsumerFunction parse, Class<T> type, Map<String, String> properties) {
-        this(groupId, parse, type, properties);
+        this(groupId, parse, properties);
         consumer.subscribe(Collections.singletonList(topic));
     }
 
     public KafkaService(String groupId, Pattern topic, ConsumerFunction parse, Class<T> type, Map<String, String> properties) {
-        this(groupId, parse, type, properties);
+        this(groupId, parse, properties);
         consumer.subscribe(topic);
     }
 
-    private KafkaService(String groupId, ConsumerFunction parse, Class<T> type, Map<String, String> properties) {
+    private KafkaService(String groupId, ConsumerFunction<T> parse, Map<String, String> properties) {
         this.parse = parse;
-        this.consumer = new KafkaConsumer<>(getProperties(type, groupId, properties));
+        this.consumer = new KafkaConsumer<>(getProperties(groupId, properties));
     }
 
     public void run() {
         while(true) {
-            ConsumerRecords<String, T> records = consumer.poll(Duration.ofMillis(100));
+            ConsumerRecords<String, Message<T>> records = consumer.poll(Duration.ofMillis(100));
 
             if (records.isEmpty()) {
                 continue;
@@ -49,7 +51,7 @@ public class KafkaService<T> {
         }
     }
 
-    private Properties getProperties(Class<T> type, String groupId, Map<String, String> overrideProperties) {
+    private Properties getProperties(String groupId, Map<String, String> overrideProperties) {
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
@@ -58,5 +60,10 @@ public class KafkaService<T> {
         properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
         properties.putAll(overrideProperties);
         return properties;
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.consumer.close();
     }
 }
